d713b6449ac97e79e5c429da8912d9700cf64031
Markus Grotz <grotz@uw.edu>
2023-05-16 14:44:20 -0700

    remove python 3.6 requirement

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
     description="Benchmark environments for high-speed robot learning in NVIDIA IsaacGym.",
     keywords=["robotics", "rl"],
     include_package_data=True,
-    python_requires=">=3.6.*",
     install_requires=INSTALL_REQUIRES,
     packages=find_packages("."),
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3.7, 3.8"],
