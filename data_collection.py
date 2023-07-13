7102a80748d149b1bd472f83b2963f1e0942eefb
UW Amazon Manipulation Project <amazon-manipulation@cs.washington.edu>
2023-07-13 12:16:06 -0700

    adding changes to files to include data path

--- data_collection.py
+++ data_collection.py
@@ -2,7 +2,10 @@ import isaacgym
 import isaacgymenvs
 import torch
 
-ne = 1
+ne = 50
+
+from pathlib import Path
+cur_path = str(Path(__file__).parent.absolute())
 
 envs = isaacgymenvs.make(
 	seed=0,
@@ -11,11 +14,13 @@ envs = isaacgymenvs.make(
 	sim_device="cuda:0",
 	rl_device="cuda:0",
 	multi_gpu=True,
-	graphics_device_id=0
+	graphics_device_id=0,
+	data_path=cur_path+"/../System_Identification_Data/Parallelization-Data/",
 )
 print("Observation space is", envs.observation_space)
 print("Action space is", envs.action_space)
 obs = envs.reset()
+
 while True:
 	action = torch.tensor(ne * [[0.1, 0, 0, 0, 0, 0, 1]])
 	obs, reward, done, info = envs.step(
