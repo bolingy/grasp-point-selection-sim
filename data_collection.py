11f3c237d674b1f7d85c9fbf6c1ae10daef3f82f
Markus Grotz <grotz@uw.edu>
2023-07-24 14:50:43 -0700

    add cli to data_collection script

--- data_collection.py
+++ data_collection.py
@@ -2,32 +2,51 @@ import isaacgym
 import isaacgymenvs
 import torch
 
-ne = 50
 
-from pathlib import Path
-cur_path = str(Path(__file__).parent.absolute())
+import click
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] ='false'
+os.environ['XLA_PYTHON_CLIENT_ALLOCATOR']='platform'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
-# Options are 3H, 3E and 3F
-bin_id = "3H"
 
-envs = isaacgymenvs.make(
-	seed=0,
-	task="UR16eManipulation",
-	num_envs=ne,
-	sim_device="cuda:0",
-	rl_device="cuda:0",
-	multi_gpu=True,
-	graphics_device_id=0,
-	headless=True,
-	bin_id=bin_id,
-	data_path=cur_path+"/../System_Identification_Data/Parallelization-Data/",
-)
-print("Observation space is", envs.observation_space)
-print("Action space is", envs.action_space)
-obs = envs.reset()
 
-while True:
-	action = torch.tensor(ne * [[0.1, 0, 0, 0, 0, 0, 1]])
-	obs, reward, done, info = envs.step(
-		action
-		)
+def _get_data_path(bin_id):
+    from datetime import datetime
+    import random
+    import string
+    datetime_string = datetime.now().isoformat().replace(":","")[:-7]
+    random_string = ''.join(random.choice(string.ascii_letters) for _ in range(6))
+    temp_path = f"~/manipulation_policy/{datetime_string}-{random_string}-grasp_data_{bin_id}/"
+    return os.path.expanduser(temp_path)
+ 
+
+@click.command()
+@click.option('--bin-id', type=click.Choice(['3H', '3E', '3F']))
+@click.option('--num-envs', default=50)
+def generate(bin_id, num_envs):
+    envs = isaacgymenvs.make(
+            seed=0,
+            task="UR16eManipulation",
+            num_envs=num_envs,
+            sim_device="cuda:0",
+            rl_device="cuda:0",
+            multi_gpu=True,
+            headless=True,
+            graphics_device_id=0,
+            bin_id=bin_id,
+            data_path=_get_data_path(bin_id),
+    )
+    print("Observation space is", envs.observation_space)
+    print("Action space is", envs.action_space)
+    obs = envs.reset()
+
+    try:
+        while True:
+            action = torch.tensor(num_envs * [[0.1, 0, 0, 0, 0, 0, 1]])
+            obs, reward, done, info = envs.step(action)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    generate()
